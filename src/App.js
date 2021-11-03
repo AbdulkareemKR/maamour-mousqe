16b5b3696db0d538712b69a4ec39daf311a18436
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-03 20:10:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 ///FOLDER NAMES MUST BE SMALL///////////
+///EXTENTION MUST BE WRITTEN////////////
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
@@ -6,8 +7,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SidebarBootstrap from "./Component/sidebar/SidebarBootstrap";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage/HomePage.js";
-import Header from "./Component/header/Header";
-import Footer from "./Component/footer/Footer";
+import Header from "./Component/header/Header.js";
+import Footer from "./Component/footer/Footer.js";
 
 function App() {
   return (
