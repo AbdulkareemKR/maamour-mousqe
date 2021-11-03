7f6b3f87b486e69c1ed510db362fa5179cbbaa89
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-03 18:34:38 +0300

    adding logo to footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import BlackLogo from "../Images/maamour-black-logo.png";
 
 function Footer() {
-  return <div className={styles.container}>info abount maamour mousqe</div>;
+  return (
+    <footer className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img
+          src={BlackLogo}
+          alt="شعار معمور"
+          className={styles.BlackLogo}
+        ></img>
+        <h3 className={styles.title}>
+          مسجد <span className={styles.titleCustom}>معمور</span>
+        </h3>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
