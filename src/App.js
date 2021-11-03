c2f74a144fb2dd521b99bc3acc900aed22f8d3ce
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-03 19:44:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import SidebarBootstrap from "./Component/sidebar/SidebarBootstrap";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/homePage/HomePage.js";
 import Header from "./Component/header/Header";
-import Footer from "./Component/footer/Footer";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <nav>
           <Header />
           <SidebarBootstrap />
-          <Footer />
         </nav>
         <Switch>
           <Route path="/registration">{/* <RegistrationPage /> */}</Route>
