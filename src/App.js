e35de0488d1a1fed1bf1425b521bd977aa2e2ff8
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-03 19:13:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-// const express = require("express");
-
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("<h1> Home Page</h1>");
-// });
-
-// app.listen(5000, () => {
-//   console.log("server started");
-// });
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SidebarBootstrap from "./Component/Sidebar/SidebarBootstrap.js";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/HomePage/HomePage.js";
 import Header from "./Component/Header/Header";
@@ -25,7 +13,6 @@ function App() {
       <div>
         <nav>
           <Header />
-          <SidebarBootstrap />
         </nav>
         <Switch>
           <Route path="/registration">{/* <RegistrationPage /> */}</Route>
