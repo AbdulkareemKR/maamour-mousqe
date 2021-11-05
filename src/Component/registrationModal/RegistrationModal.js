b850cc27957e4c2ee6f82b63312d923b8a878f99
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-05 23:30:08 +0300

    reg logo

--- src/Component/registrationModal/RegistrationModal.js
+++ src/Component/registrationModal/RegistrationModal.js
@@ -8,6 +8,7 @@ import { HiTag } from "react-icons/hi";
 import { MdEmail } from "react-icons/md";
 import { AiFillUnlock } from "react-icons/ai";
 import Container from "react-bootstrap/Container";
+import brownLogo from "../images/brown-logo.png";
 
 // import React, { useState } from "react";
 
@@ -15,151 +16,159 @@ function RegistrationModal(props) {
   return (
     <div>
       <Modal
-        className={styles.modal}
         show={props.showModal}
         onHide={props.handleClose}
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-        <Modal.Header className={styles.title}>
-          <Modal.Title
-            className={styles.title}
-            id="contained-modal-title-vcenter"
-          >
-            {props.logIn ? "تسجيل الدخول " : " إنشاء حساب "}
-            <BsFillPeopleFill className={styles.icons} />
-          </Modal.Title>
-        </Modal.Header>
-        <Row className={styles.registrationButtons}>
-          <Col>
-            <Button
-              className={`${styles.createButton} ${
-                props.logIn ? "" : `${styles.createButtonActive}`
-              }`}
-              onClick={props.handleLogOut}
+        <img
+          src={brownLogo}
+          alt="شعار بني"
+          className={styles.backgroundLogo}
+        ></img>
+        <div className={styles.containter}>
+          <Modal.Header className={styles.title}>
+            <Modal.Title
+              className={styles.title}
+              id="contained-modal-title-vcenter"
             >
-              إنشاء حساب
-            </Button>
-          </Col>
-          <Col>
-            <Button
-              className={`${styles.createButton} ${
-                props.logIn ? `${styles.createButtonActive}` : ""
-              }`}
-              onClick={props.handleLogIn}
-            >
-              تسجيل دخول
-            </Button>
-          </Col>
-        </Row>
-        <Form className={styles.formStyle}>
+              {props.logIn ? "تسجيل الدخول " : " إنشاء حساب "}
+              <BsFillPeopleFill className={styles.icons} />
+            </Modal.Title>
+          </Modal.Header>
           <Container fluid style={{ margin: "auto" }}>
-            <Modal.Body className="show-grid">
-              {props.logIn ? (
-                ""
-              ) : (
-                <Form.Group as={Row} className={styles.group}>
-                  <Form.Label className={styles.label} column sm="4">
-                    <FaIdCard className={styles.icons} /> الاسم
-                  </Form.Label>
-                  <Col>
-                    <Form.Control
-                      required
-                      className={styles.input}
-                      type="text"
-                      placeholder="أدخل اسمك"
-                    />
-                  </Col>
-                </Form.Group>
-              )}
-              <Form.Group as={Row} className={styles.group}>
-                <Form.Label className={styles.label} column sm="4">
-                  <MdEmail className={styles.icons} /> الايميل
-                </Form.Label>
-                <Col>
-                  <Form.Control
-                    required
-                    className={styles.input}
-                    type="email"
-                    placeholder="أدخل ايميلك"
-                  />
-                </Col>
-              </Form.Group>
-
-              <Form.Group as={Row} className={styles.group}>
-                <Form.Label className={styles.label} column sm="4">
-                  <AiFillUnlock className={styles.icons} /> كلمة المرور
-                </Form.Label>
-                <Col>
-                  <Form.Control
-                    required
-                    className={styles.input}
-                    type="password"
-                    placeholder="أدخل كلمة المرور"
-                  />
-                </Col>
-              </Form.Group>
-
-              {props.logIn ? (
-                ""
-              ) : (
-                <div>
+            <Row className={styles.registrationButtons}>
+              <Col>
+                <Button
+                  className={`${styles.createButton} ${
+                    props.logIn ? "" : `${styles.createButtonActive}`
+                  }`}
+                  onClick={props.handleLogOut}
+                >
+                  إنشاء حساب
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  className={`${styles.createButton} ${
+                    props.logIn ? `${styles.createButtonActive}` : ""
+                  }`}
+                  onClick={props.handleLogIn}
+                >
+                  تسجيل دخول
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+          <Form className={styles.formStyle}>
+            <Container fluid style={{ margin: "auto" }}>
+              <Modal.Body className="show-grid">
+                {props.logIn ? (
+                  ""
+                ) : (
                   <Form.Group as={Row} className={styles.group}>
                     <Form.Label className={styles.label} column sm="4">
-                      <AiFillUnlock className={styles.icons} />
-                      تأكيد كلمة المرور
+                      <FaIdCard className={styles.icons} /> الاسم
                     </Form.Label>
                     <Col>
                       <Form.Control
                         required
                         className={styles.input}
-                        type="password"
-                        placeholder="أعد ادخال كلمة المرور"
+                        type="text"
+                        placeholder="أدخل اسمك"
                       />
                     </Col>
                   </Form.Group>
-                  <Form.Group as={Row} className={styles.group}>
-                    <Form.Label className={styles.label} column sm="4">
-                      <HiTag className={styles.icons} /> المرحلة الدراسية
-                    </Form.Label>
-                    <Col>
-                      <Form.Control as="select" className={styles.input}>
-                        <option>دون الابتدائي</option>
-                        <option>ابتدائي</option>
-                        <option>متوسط</option>
-                        <option>ثانوي</option>
-                        <option>جامعي</option>
-                        <option>أب</option>
-                      </Form.Control>
-                    </Col>
-                  </Form.Group>
-                </div>
-              )}
-              <a className={styles.forget} href={"/"}>
-                هل نسيت كلمة المرور؟
-              </a>
-            </Modal.Body>
-            <Modal.Footer className={styles.footer}>
-              {props.logIn ? (
-                <Button
-                  className={`${styles.createButton} ${styles.submit}`}
-                  type="submit"
-                  // onClick={() => setModalShow(false)}
-                >
-                  سجل دخول
-                </Button>
-              ) : (
-                <Button
-                  className={`${styles.createButton} ${styles.submit}`}
-                  type="submit"
-                  // onClick={() => setModalShow(false)}
-                >
-                  أنشئ الحساب
-                </Button>
-              )}
-            </Modal.Footer>
-          </Container>
-        </Form>
+                )}
+                <Form.Group as={Row} className={styles.group}>
+                  <Form.Label className={styles.label} column sm="4">
+                    <MdEmail className={styles.icons} /> الايميل
+                  </Form.Label>
+                  <Col>
+                    <Form.Control
+                      required
+                      className={styles.input}
+                      type="email"
+                      placeholder="أدخل ايميلك"
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className={styles.group}>
+                  <Form.Label className={styles.label} column sm="4">
+                    <AiFillUnlock className={styles.icons} /> كلمة المرور
+                  </Form.Label>
+                  <Col>
+                    <Form.Control
+                      required
+                      className={styles.input}
+                      type="password"
+                      placeholder="أدخل كلمة المرور"
+                    />
+                  </Col>
+                </Form.Group>
+
+                {props.logIn ? (
+                  ""
+                ) : (
+                  <div>
+                    <Form.Group as={Row} className={styles.group}>
+                      <Form.Label className={styles.label} column sm="4">
+                        <AiFillUnlock className={styles.icons} />
+                        تأكيد كلمة المرور
+                      </Form.Label>
+                      <Col>
+                        <Form.Control
+                          required
+                          className={styles.input}
+                          type="password"
+                          placeholder="أعد ادخال كلمة المرور"
+                        />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} className={styles.group}>
+                      <Form.Label className={styles.label} column sm="4">
+                        <HiTag className={styles.icons} /> المرحلة الدراسية
+                      </Form.Label>
+                      <Col>
+                        <Form.Control as="select" className={styles.input}>
+                          <option>دون الابتدائي</option>
+                          <option>ابتدائي</option>
+                          <option>متوسط</option>
+                          <option>ثانوي</option>
+                          <option>جامعي</option>
+                          <option>أب</option>
+                        </Form.Control>
+                      </Col>
+                    </Form.Group>
+                  </div>
+                )}
+                <a className={styles.forget} href={"/"}>
+                  هل نسيت كلمة المرور؟
+                </a>
+              </Modal.Body>
+              <Modal.Footer className={styles.footer}>
+                {props.logIn ? (
+                  <Button
+                    className={`${styles.createButton} ${styles.submit}`}
+                    type="submit"
+                    // onClick={() => setModalShow(false)}
+                  >
+                    سجل دخول
+                  </Button>
+                ) : (
+                  <Button
+                    className={`${styles.createButton} ${styles.submit}`}
+                    type="submit"
+                    // onClick={() => setModalShow(false)}
+                  >
+                    أنشئ الحساب
+                  </Button>
+                )}
+              </Modal.Footer>
+            </Container>
+          </Form>
+        </div>
       </Modal>
     </div>
   );
