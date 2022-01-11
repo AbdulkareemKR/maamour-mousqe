20f78ba65ae28dc4c1362ff0ef6af0a2be38ea2c
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-01-11 20:59:34 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// mysql://bbb32223c9ea97:303c0a01@us-cdbr-east-04.cleardb.com/heroku_35584d16ac6b3a9?reconnect=true
 const express = require("express");
 const app = express();
 const cors = require("cors");
