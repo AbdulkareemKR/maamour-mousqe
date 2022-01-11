9f964650ca7024c566c0f00fcd86ed27e03d0f84
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-01-11 14:35:58 +0300

    customizing not found page

--- client/src/pages/pageNotFound/PageNotFound.js
+++ client/src/pages/pageNotFound/PageNotFound.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./pageNotFound.module.css";
 
 function PageNotFound() {
-  return <div>not found</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        <span className={styles.customText}>... </span>قريبا
+      </p>
+    </div>
+  );
 }
 
 export default PageNotFound;
