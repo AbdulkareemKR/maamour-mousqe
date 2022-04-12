2d7c45f55acb2bed50565cd52acd1a2c0c5d36e0
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-04-12 17:03:29 +0300

    a

--- client/src/pages/iftarProgram/Iftar.jsx
+++ client/src/pages/iftarProgram/Iftar.jsx
@@ -11,7 +11,7 @@ function Iftar() {
           
           <Container fluid>
   <Row>
-    <Col><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeUvYF4UsVPV8fqGp2wwftXWt-ORJUl7GaZXeNzrkor6kbT4A/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>  </Col>
+    <Col><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeUvYF4UsVPV8fqGp2wwftXWt-ORJUl7GaZXeNzrkor6kbT4A/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>  </Col>
   </Row>
 </Container>
     </div>
